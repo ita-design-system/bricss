83ed577820c57f8b6df56b0ab48b910837495577
Olivier Blanc <olivier3lanc@gmail.com>
2024-07-19 09:59:29 +0200

    clean

--- assets/sandbox/js/__sandbox.js
+++
@@ -1,9 +0,0 @@
-// ONLY FOR SANDBOX PURPOSE
-const sandbox = {
-    update: function() {
-        // Include newest CSS
-        const includedStyleMarkup = `<link rel="stylesheet" href="${window.parent.dsgCssFile.url}">`;
-        document.head.insertAdjacentHTML('beforeend', includedStyleMarkup);
-    }
-}
-sandbox.update();
